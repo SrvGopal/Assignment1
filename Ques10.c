ed5279d5fb771bf0c531abf69f334c3f07200f73
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-25 23:24:17 +0530

    Create Ques10.c

---
+++ Ques10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float radius , volume;
+
+    printf("Enter the radius of sphere here");
+    scanf("%f",&radius);
+
+    volume = (4*3.1415926*(radius*radius*radius))/3;
+
+    printf("The volume of the sphere is %f ",volume);
+
+}
+

3fb42567fb16dc8b8a5e5b70e9e27d2f2a44c739
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-25 23:23:47 +0530

    Create Ques9.c

---
+++ Ques9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int days , year ,weeks ;
+
+    printf("Enter the total days ");
+    scanf("%d",&days);
+
+    year = days / 365;
+    weeks = (days /7 )%52;
+    days = days / 365;
+
+    printf("It is %d Years: %d weeks: %d days",
+    year,weeks,days);
+
+}
